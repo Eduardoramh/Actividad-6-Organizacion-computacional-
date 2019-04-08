dbeac0295de4ef109471a22f3cf04ce03d73b868
Eduardoramh <lalo.ramirezh102@gmail.com>
2019-04-08 11:10:24 -0500

    Cuadrado

---
+++ src/actividad/pkg6/Cuadrado.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad.pkg6;
+
+/**
+ *
+ * @author lalor
+ */
+public class Cuadrado {
+        public static void main(String[] args) {
+System.out.println("                          Figura geometrica: Cuadrado                                       ");
+System.out.println("");
+System.out.println("                          ***************************");
+System.out.println("                          *                         *");
+System.out.println("                          *                         *");
+System.out.println("                          *                         *");
+System.out.println("                          *                         *");
+System.out.println("                          *                         *");
+System.out.println("                          *                         *");
+System.out.println("                          *                         *");
+System.out.println("                          *                         *");
+System.out.println("                          *                         *");
+System.out.println("                          *                         *");
+System.out.println("                          ***************************");
+                                                                                             
+        }
+}
