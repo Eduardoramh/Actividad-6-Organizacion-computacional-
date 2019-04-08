e5609028f32f1a97703affbf12e1b939b6df43fa
Eduardoramh <lalo.ramirezh102@gmail.com>
2019-04-08 11:42:03 -0500

    Triangulo

--- src/actividad/pkg6/Rectangulo.java
+++ src/actividad/pkg6/Rectangulo.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package actividad.pkg6;
 
 /**
@@ -11,7 +6,7 @@ package actividad.pkg6;
  */
 public class Rectangulo {
     public static void main(String[] args) {
-System.out.println("                                     Figura geometrica: Rectangulo                                       ");
+System.out.println("                                      Figura geometrica: Rectangulo                                       ");
 System.out.println("");
 System.out.println("                          ***************************************************");
 System.out.println("                          *                                                 *");
